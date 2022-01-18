91d4ed440ade498116cf2712b680c366499687fd
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-01-18 10:32:12 +0530

    Time: 20 ms (47.51%), Space: 20.4 MB (20.95%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -2,95 +2,57 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& fl, int n) {
         
-       int cnt1=0;
+       int cnt=0;
         
         int size=fl.size();
-       for(int i=0;i<size;i+=2)
-       {
-           if(fl[i]==0)
-           {
-               if(i==0)
-               {
-                   if(size>=2)
-                   {
-                       if(fl[i+1]!=1)
-                       {
-                           cnt1++;
-                       }
-                   }
-                   else
-                   {
-                       cnt1++;
-                   }
-               }
-               else if(i==size-1)
-               {
-                   if(size>=2)
-                   {
-                       if(fl[i-1]!=1)
-                       {
-                           cnt1++;
-                       }
-                   }
-                   else
-                   {
-                       cnt1++;
-                   }
-               }
-               else
-               {
-                   if(fl[i]==0&&fl[i-1]==0&&fl[i+1]==0)
-                       cnt1++;
-               }
-           }
-       }
         
-        int cnt2=0;
-           for(int i=1;i<size;i+=2)
-       {
-           if(fl[i]==0)
-           {
-               if(i==0)
-               {
-                   if(size>=2)
-                   {
-                       if(fl[i+1]!=1)
-                       {
-                           cnt2++;
-                       }
-                   }
-                   else
-                   {
-                       cnt2++;
-                   }
-               }
-               else if(i==size-1)
-               {
-                   if(size>=2)
-                   {
-                       if(fl[i-1]!=1)
-                       {
-                           cnt2++;
-                       }
-                   }
-                   else
-                   {
-                       cnt1++;
-                   }
-               }
-               else
-               {
-                   if(fl[i]==0&&fl[i-1]==0&&fl[i+1]==0)
-                       cnt2++;
-               }
-           }
-       }
+        for(int i=0;i<size;i++)
+        {
+            if(fl[i]==0)
+            {
+                if(i==0)
+                {
+                    if(i+1<size)
+                    {
+                    if(fl[i+1]==0)
+                    {
+                        cnt++;
+                        fl[i]=1;
+                    }
+                    }
+                    else
+                    {
+                        cnt++;
+                    }
+                    
+                }
+                else if(i==size-1)
+                {
+                     if(i-1>=0)
+                    {
+                    if(fl[i-1]==0)
+                    {
+                        cnt++;
+                        fl[i]=1;
+                    }
+                     }
+                    else
+                    {
+                        cnt++;
+                    }
+                }
+                else
+                {
+                    if(fl[i-1]==0&&fl[i+1]==0){
+                        fl[i]=1;
+                        cnt++;
+                    }
+                }
+            }
+        }
         
-        int k=max(cnt1,cnt2);
-        
-        if(k>=n)
-            return true;
-        
-        return false;
+      
+        return cnt>=n;
+       
     }
 };
