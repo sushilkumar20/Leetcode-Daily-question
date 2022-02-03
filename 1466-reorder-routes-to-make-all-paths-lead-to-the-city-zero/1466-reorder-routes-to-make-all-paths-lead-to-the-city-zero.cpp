dcee9ce52029da2b71fcaaa9ab3083da6eb73f65
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-02-03 19:48:50 +0530

    Time: 665 ms (37.76%), Space: 112.8 MB (58.98%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void dfs(vector<int>graph[],vector<int>&vis,int &ans ,int node)
+    {
+        if(vis[node])
+           return;
+         vis[node]=1;
+        
+         vector<int> neighbour=graph[node];
+        
+         for(int i=0;i<neighbour.size();i++)
+         {
+              if(vis[abs(neighbour[i])]==0&&neighbour[i]>0)
+                 ans++;
+             int k=abs(neighbour[i]);
+             dfs(graph,vis,ans,k);
+            
+         }
+    }
+    int minReorder(int n, vector<vector<int>>& connections) {
+        
+        vector<int> graph[n];
+        
+        for(int i=0;i<connections.size();i++)
+        {
+            graph[connections[i][0]].push_back(connections[i][1]);
+            graph[connections[i][1]].push_back(-1*connections[i][0]);
+        }
+        
+        vector<int> vis(n,0);
+        
+        int ans=0;
+       
+        dfs(graph,vis,ans,0);
+        
+        return ans;
+    }
+       bool needsToPointInReverseDirection(int node)
+       {
+        if(node > 0) return true;
+        return false;
+       }
+};
