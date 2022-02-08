4a995dcf5d2313801b3a417b8e13bb17734914df
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-02-08 11:20:01 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.37%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        
+        while(num>=10)
+        {
+            int ans=0;
+             while(num!=0)
+             {
+                 ans+=num%10;
+                 num=num/10;
+             }
+            
+            num=ans;
+        }
+        
+        return num;
+    }
+};
