b9f4e96d436272216c2ac6188a345d6cab8cb483
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-04-29 02:12:26 +0530

    Time: 818 ms (13.92%), Space: 77.7 MB (10.79%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,76 +1,47 @@
 class Solution {
 public:
-    bool bfs(int i,int j, vector<vector<int>>& heights,int mid)
-    {
-        int n = heights.size();
-        int m = heights[0].size();
-        queue<pair<int,int>> q;
-        
-        vector<vector<int>> dir ={{1,0},{0,1},{-1,0},{0,-1}};
-        
-        q.push({0,0});
-        
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        vis[0][0]=1;
-        while(q.size())
-        {
-            auto p = q.front();
-            q.pop();
-            
-            
-            int x = p.first;
-            int y = p.second;
-            
-            if(x==n-1&&y==m-1)
-                return true;
-            
-            for(auto &d :dir)
-            {
-                int x1=x+d[0];
-                int y1=y+d[1];
-                
-                if(x1<0||y1<0||x1>=n||y1>=m)
-                {
-                    continue;
-                }
-                
-             
-                
-                if(vis[x1][y1])
-                    continue;
-                if(abs(heights[x][y]-heights[x1][y1])>mid)
-                    continue;
-                
-                 if(x1==n-1&&y1==m-1)
-                  return true;
-                vis[x1][y1]=1;
-                q.push({x1,y1});
-                
-            }
-        }
-        return false;
-    }
+   
     int minimumEffortPath(vector<vector<int>>& heights) {
         
-        int low =0;
-        int high =1e9;
-        int ans=1e9;
-       
-        cout<<bfs(0,0,heights,1)<<endl;
-        while(low<=high){
+        int n = heights.size();
+        int m = heights[0].size();
+        
+        vector<vector<int>> dir ={{1,0},{-1,0},{0,1},{0,-1}};
+        vector<vector<int>> dis(n,vector<int>(m,INT_MAX));
+        
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+        
+        pq.push({0,0,0});
+        
+        dis[0][0]=0;
+        
+        while(pq.size())
+        {
+            auto p =pq.top();
+            pq.pop();
             
-            int mid =(low+high)/2;
-            if(bfs(0,0,heights,mid))
+            int x = p[1];
+            int y = p[2];
+            
+            if(dis[x][y]<p[0]) continue;
+           // if(x==n-1&&y==m-1) return p[0];
+           
+            
+            for(auto i:dir)
             {
-                ans =  min(mid,ans);
-                high = mid-1;
-            }
-            else
-            {
-                low =  mid+1;
+                int x1 = x+i[0];
+                int y1 = y+i[1];
+                
+                if(x1>=n||x1<0||y1>=m||y1<0)
+                    continue;
+                
+                if(dis[x1][y1]>max(p[0],abs(heights[x][y]-heights[x1][y1]))){
+                    dis[x1][y1]=max(p[0],abs(heights[x][y]-heights[x1][y1]));
+                    pq.push({dis[x1][y1],x1,y1});
+                }
             }
         }
         
-        return ans;
+        return dis[n-1][m-1];
     }
 };
