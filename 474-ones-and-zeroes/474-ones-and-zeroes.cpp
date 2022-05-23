eec9f11e808b1ddea62dd24d756ef82abaa3fe42
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-05-24 01:07:57 +0530

    Time: 346 ms (56.08%), Space: 105.7 MB (5.37%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,19 +1,56 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
-    int fun(int i, int m, int n, vector<pair<int, int>>& v){
-        if(i<0) return 0;
-        if(dp[i][m][n]!=-1) return dp[i][m][n];
-        if(m>=v[i].first && n>=v[i].second) return dp[i][m][n] = max(1+fun(i-1, m-v[i].first, n-v[i].second, v), fun(i-1, m, n, v));
-        return dp[i][m][n] = fun(i-1, m, n, v);
+     vector<vector<vector<int>>> dp;
+    int fun(vector<int>&one, vector<int> &zero, int m,int n, int i)
+    {
+        if(i<=0){
+            
+            return 0;
+        }
+        
+        if(dp[i][m][n] != -1)
+            return dp[i][m][n];
+        
+        if(zero[i-1]<=m&&one[i-1]<=n)
+        {
+            
+            dp[i][m][n] = max(fun(one,zero,m,n,i-1), 1+fun(one,zero,m-zero[i-1],n-one[i-1],i-1));
+        }
+        else
+        {
+             dp[i][m][n] = fun(one,zero,m,n,i-1);
+        }
+        
+        return dp[i][m][n];
     }
     int findMaxForm(vector<string>& strs, int m, int n) {
-        dp.resize(strs.size()+2, vector<vector<int>>(m+2, vector<int>(n+2, -1)));
-        vector<pair<int, int>> v;
-        for(string x : strs){
-            int cnt = count(x.begin(), x.end(), '0');
-            v.push_back({cnt, x.size()-cnt});
+        
+        int sz = strs.size();
+        vector<int> zero;
+        vector<int> one;
+        
+        for(int i=0;i<strs.size();i++)
+        {
+            int z=0;
+            int o=0;
+            
+            for(int j=0;j<strs[i].size();j++)
+            {
+                if(strs[i][j] == '0')
+                    z++;
+                else
+                    o++;
+            }
+            
+            zero.push_back(z);
+            one.push_back(o);
         }
-        return fun(strs.size()-1, m, n, v);
+         int curr=0;
+        int ans=0;
+        
+dp.resize(strs.size()+2, vector<vector<int>>(m+2, vector<int>(n+2, -1)));           
+        
+        return  fun(one,zero,m,n,sz);;
+        
     }
 };
