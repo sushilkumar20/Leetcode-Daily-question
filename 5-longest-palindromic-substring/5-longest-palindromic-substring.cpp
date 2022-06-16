55a139f697242731b6622282278ccd1841eb13ef
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-06-17 00:33:11 +0530

    Time: 740 ms (16.31%), Space: 386.1 MB (8.97%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -3,40 +3,49 @@ public:
     string longestPalindrome(string s) {
         
         int n=s.size();
-        string s1=s;
+        vector<vector<int>> dp(n,vector<int>(n,0));
         
-        reverse(s1.begin(),s1.end());
-        
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        int mx =0;
         string ans ="";
-        for(int i=1;i<=n;i++)
+        int mx=0;
+        
+        for(int i=0;i<n;i++)
         {
-            for(int j=1;j<=n;j++)
+            ans=s[i];
+            mx=1;
+            
+            dp[i][i]=1;
+        }
+        
+        for(int i=0;i<n-1;i++)
+        {
+            if(s[i]==s[i+1])
             {
-                if(s[i-1]==s1[j-1])
+                dp[i][i+1]=1;
+                ans=s.substr(i,2);
+                mx=2;
+            }
+        }
+        
+        for(int k=3;k<=n;k++)
+        {
+            for(int i=0;i<=n-k;i++)
+            {
+                int j = i+k-1;
+                //cout<<i<<" "<<j<<endl;
+                if(s[i]==s[j]&&dp[i+1][j-1])
                 {
-                    dp[i][j] = dp[i-1][j-1]+1;
-                    if(mx<dp[i][j])
-                    {
-                        string s2=s.substr(i-dp[i][j],dp[i][j]);
-                        string s3=s2;
-                        reverse(s3.begin(),s3.end());
-                        if(s2==s3)
-                        {
-                            ans=s.substr(i-dp[i][j],dp[i][j]);
-                            mx=dp[i][j];
-                        }
-                    }
+                    dp[i][j]=1;
                     
-                }
-                else
-                {
-                    dp[i][j]=0;
+                    if(mx<k)
+                    {
+                        mx=k;
+                       
+                        ans=s.substr(i,k);
+                    }
                 }
             }
         }
         
-       return ans;
+        return ans;
     }
 };
