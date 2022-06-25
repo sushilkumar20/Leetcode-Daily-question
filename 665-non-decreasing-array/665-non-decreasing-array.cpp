439b75c5c58296d1a6cb2371670c8544462669eb
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-06-25 12:28:58 +0530

    Time: 59 ms (13.17%), Space: 27 MB (53.59%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -1,55 +1,27 @@
 class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
-        int n= nums.size();
-       vector<int> pref(n);
-        vector<int> suff(n);
+       
+        int n=nums.size();
+        int cnt=0;
         
-        if(n==1)
-            return true;
-        
-        pref[0]=1;
         for(int i=1;i<n;i++)
         {
-            if(nums[i]>=nums[i-1])
+            if(nums[i]<nums[i-1])
             {
-                pref[i]=(pref[i-1]&1);
-            }
-            else
-                pref[i]=0;
-        }
-        
-        suff[n-1]=1;
-        
-        for(int i=n-2;i>=0;i--)
-        {
-             if(nums[i]<=nums[i+1])
-            {
-                suff[i]=(suff[i+1]&1);
-            }
-            else
-                suff[i]=0;
-        }
-        
-        for(int i=0;i<n;i++)
-        {
-            if(i==0)
-            {
-                if(suff[i+1])
-                    return true;
-            }
-            else if(i==n-1)
-            {
-                if(pref[i-1])
-                    return true;
-            }
-            else 
-            {
-                if(pref[i-1]&&suff[i+1]&&nums[i-1]<=nums[i+1])
-                    return true;
+                cnt++;
+                if(cnt>1)
+                    return false;
+                
+                if(i==1||nums[i-2]<=nums[i])
+                {
+                    nums[i-1]=nums[i];
+                }
+                else
+                    nums[i]=nums[i-1];
             }
         }
         
-        return false;
+        return true;
     }
 };
