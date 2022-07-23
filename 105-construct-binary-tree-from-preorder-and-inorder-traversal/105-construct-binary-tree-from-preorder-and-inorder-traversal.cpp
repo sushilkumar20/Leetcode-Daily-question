fcfac2b024cbceee470e92b588ad825f1d57b8c2
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-07-23 14:41:08 +0530

    Time: 47 ms (37.11%), Space: 26.3 MB (58.14%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,36 +11,32 @@
  */
 class Solution {
 public:
-    TreeNode* construct(vector<int>& preorder,int &ps, vector<int>&inorder, int is, int ie,map<int,int> &mp)
+    
+    TreeNode*construct(int &ind, vector<int>&preorder,map<int,int>&mp,vector<int>&inorder,int low, int high)
     {
-        if(is>ie||ps>=preorder.size())
+        if(low>high||ind>preorder.size())
             return nullptr;
         
-        TreeNode * root = new TreeNode(preorder[ps]);
+        TreeNode *root = new TreeNode(preorder[ind]);
+        int mid = mp[preorder[ind]];
+        ind++;
         
-        int k=mp[preorder[ps]];
-        
-       // int fromStartInorder = k-is;
-        ps++;
-        root->left = construct(preorder,ps,inorder,is,k-1,mp);
-      //  ps++;
-        root->right = construct(preorder,ps,inorder,k+1,ie,mp);
+        root->left=construct(ind,preorder,mp,inorder,low,mid-1);
+        root->right=construct(ind,preorder,mp,inorder,mid+1,high);
         
         return root;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         
-        if(preorder.size()!=inorder.size())
-            return nullptr;
-        
-        int n = inorder.size();
         map<int,int> mp;
-        for(int i=0;i<n;i++)
+        
+        for(int i=0;i<inorder.size();i++)
         {
-            mp[inorder[i]]=i;
+            mp[inorder[i]] = i;
         }
         
-        int i=0;
-        return construct(preorder,i,inorder,0,inorder.size()-1,mp);
+        int ind =0;
+        
+        return construct(ind, preorder,mp,inorder,0,inorder.size()-1);
     }
 };
