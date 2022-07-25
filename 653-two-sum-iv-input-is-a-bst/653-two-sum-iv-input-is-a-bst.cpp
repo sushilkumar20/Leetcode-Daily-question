9dc39d76e8f174247aecc5ce10c3585526626f24
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-07-26 00:44:17 +0530

    Time: 63 ms (53.95%), Space: 36.9 MB (43.31%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -9,30 +9,84 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTIterator{
+    public:
+    stack<TreeNode*> st;
+    bool reverse;
+    
+    BSTIterator(TreeNode *root, bool reverse)
+    {
+        this->reverse = reverse;
+        pushAll(root);
+    }
+    
+    bool hasNext()
+    {
+        return st.size();
+    }
+    
+    int next()
+    {
+        TreeNode *top=st.top();
+        st.pop();
+        int k=top->val;
+        
+        if(reverse)
+        {
+            if(top->left)
+            {
+                pushAll(top->left);
+            }
+        }else
+        {
+            if(top->right)
+            {
+                pushAll(top->right);
+            }
+        }
+        
+        return k;
+    }
+    
+    void pushAll(TreeNode *root)
+    {
+        while(root)
+        {
+            st.push(root);
+            if(reverse)
+                root=root->right;
+            else
+                root=root->left;
+        }
+    }
+};
 class Solution {
 public:
-    bool find(TreeNode *root, int k, map<int,int>&mp)
-    {
-        if(root==nullptr)
-            return false;
-        
-        int x = k-root->val;
-        
-        if(mp.find(x)!=mp.end())
-            return true;
-        
-        mp[root->val]++;
-        
-        if(find(root->left,k,mp)||find(root->right,k,mp))
-            return true;
-        
-        return false;
-        
-    }
     bool findTarget(TreeNode* root, int k) {
         
-        map<int,int> mp;
+       // map<int,int> mp;
         
-       return find(root,k,mp);
+        BSTIterator *l = new BSTIterator(root,false);
+        BSTIterator *r = new BSTIterator(root,true);
+        
+        int i= l->next();
+        int j= r->next();
+        //cout<<i<<" "<<j<<endl;
+        while(i<j)
+        {
+           // cout<<i<<" "<<j<<endl;
+            if(i+j==k)
+                return true;
+            else if(i+j>k)
+            {
+                j=r->next();
+            }
+            else
+            {
+                i=l->next();
+            }
+        }
+        
+       return false;
     }
 };
