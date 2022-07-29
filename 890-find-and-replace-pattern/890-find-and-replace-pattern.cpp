866a99f1424778fe7102b53946b67787b158df53
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-07-29 17:14:50 +0530

    Time: 9 ms (34.36%), Space: 8.6 MB (43.97%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -2,35 +2,57 @@ class Solution {
 public:
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
         
+        
         vector<string> ans;
         
         for(int i=0;i<words.size();i++)
         {
-            bool ok=true;
+            if(pattern.size()!=words[i].size())
+                continue;
             
-            map<char,char>mp1;
-            map<char,char> mp2;
-            for(int j=0;j<words[i].size();j++)
+            map<char,char> mp;
+            map<char,char> mp1;
+            bool ok = true;
+            
+            for(int j=0;j<pattern.size();j++)
             {
-                if(mp1.find(words[i][j])==mp1.end()&&mp2.find(pattern[j])==mp2.end())
+                if(mp.find(pattern[j])==mp.end()&&mp1.find(words[i][j])==mp1.end())
                 {
-                    mp1[words[i][j]]=pattern[j];
-                    mp2[pattern[j]]=words[i][j];
+                    mp[pattern[j]] = words[i][j];
+                    mp1[ words[i][j]] = pattern[j];
                 }
                 else
-                   {
-                       if(mp1[words[i][j]]!=pattern[j]||mp2[pattern[j]]!=words[i][j])
-                       {
-                           ok=false;
-                           break;
-                           
-                       }
-                   }
+                {
+                   // cout<<words[i]<<" "<<words[i][j]<<endl;
+                    if(mp1.find(words[i][j])==mp1.end())
+                    {
+                        ok=false;
+                        break;
+                    }
+                    
+                    if(mp.find(pattern[j])==mp.end())
+                    {
+                        ok=false;
+                        break;
+                    }
+                    if(mp[pattern[j]]!=words[i][j])
+                    {
+                        ok=false;
+                        break;
+                    }
+                    
+                    if(mp1[words[i][j]]!=pattern[j])
+                    {
+                        ok=false;
+                        break;
+                    }
+                }
             }
-                   if(ok)
-                   ans.push_back(words[i]);
+            
+            if(ok)
+                ans.push_back(words[i]);
         }
-                   
-                   return ans;
+        
+        return ans;
     }
 };
