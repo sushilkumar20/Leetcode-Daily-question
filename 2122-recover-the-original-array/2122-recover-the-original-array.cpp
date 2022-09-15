91af56b4f79bc82d17bfa1fa48e908b10153f32b
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-09-15 20:40:31 +0530

    Time: 795 ms (64.39%), Space: 53.6 MB (65.53%) - LeetHub

--- 2122-recover-the-original-array/2122-recover-the-original-array.cpp
+++ 2122-recover-the-original-array/2122-recover-the-original-array.cpp
@@ -3,38 +3,66 @@ public:
     vector<int> recoverArray(vector<int>& nums) {
         
         int n = nums.size();
-        
         sort(nums.begin(),nums.end());
         
+        unordered_map<int,int> mp;
+        
+        
         for(int i=0;i<n;i++)
+            mp[nums[i]]++;
+        
+        unordered_map<int,int> mp1 = mp;
+        
+        set<int> arrk;
+        
+        for(int i=0;i<nums.size();i++)
         {
-            int k = nums[i]-nums[0];
-            
-            if(k!=0&&k%2==0)
+           int diff = abs(nums[i]-nums[0]);
+          //  cout<<diff<<endl;
+            if(diff!=0&&diff%2==0)
             {
-                queue<int> q;
-                vector<int> ans;
-                q.push(nums[0]);
-                
-                for(int j=1;j<n;j++)
-                {
-                    if(nums[j]-k==q.front())
-                    {
-                        q.pop();
-                        
-                        ans.push_back(nums[j]-k/2);
-                    }else
-                    {
-                        q.push(nums[j]);
-                    }
-                }
-                
-                if(q.empty())
-                    return ans;
+                arrk.insert(diff/2);
             }
         }
         
-        return {};
+        while(arrk.size())
+        {
+            int k = *arrk.begin();
+            arrk.erase(arrk.begin());
+            
+            
+            vector<int> arr1;
+            bool ok=true;
+            for(int i=0;i<n;i++)
+            {
+                if(mp.find(nums[i])==mp.end())
+                    continue;
+                if(mp.find(nums[i]+2*k)!=mp.end())
+                {
+                    arr1.push_back(nums[i]+k);
+                    mp[nums[i]]--;
+                    mp[nums[i]+2*k]--;
+                    
+                    if(mp[nums[i]]==0)
+                        mp.erase(nums[i]);
+                     if(mp[nums[i]+2*k]==0)
+                        mp.erase(nums[i]+2*k);
+                }
+                else
+                {
+                    //cout<<nums[i]<<endl;
+                    ok=false;
+                    break;
+                }
+            }
+            
+            if(ok)
+                return arr1;
+            
+            mp=mp1;
+        }
+            
         
+        return {};
     }
 };
