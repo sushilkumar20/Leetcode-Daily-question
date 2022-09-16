53376f3f211acd5f8c0a3b828a71da77c66b64b1
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-09-16 17:34:31 +0530

    Time: 554 ms (5.58%), Space: 116.3 MB (5.43%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
-    int dfs(int i,int t,vector<int>& piles,vector<vector<int>> &dp)
+    
+    int dfs(int i, int j, int t, vector<int>& piles,  vector<vector<vector<int>>> &dp)
     {
-        if(i>=piles.size())
+        if(i>j)
             return 0;
         
-        if(dp[i][t]!=-1)
-            return dp[i][t];
-        int k = piles[i]+dfs(i+1,(t+1)%2,piles,dp);
+        if(dp[i][j][t]!=-1)
+            return dp[i][j][t];
         
-        int x = piles.back();
-        piles.pop_back();
-        
-        int k1 = x+dfs(i,(t+1)%2,piles,dp);
-        piles.push_back(x);
-        return dp[i][t] = max(k,k1);
+        if(t==0)
+        {
+            int left = piles[i]+dfs(i+1,j,1,piles,dp);
+            int right = piles[j]+dfs(i,j-1,1,piles,dp);
+            
+            return dp[i][j][0] = max(left,right);
+        }
+        else
+        {
+            int left = dfs(i+1,j,0,piles,dp);
+            int right = dfs(i,j-1,0,piles,dp);
+            
+            return dp[i][j][0] = min(left,right);
+        }
     }
     bool stoneGame(vector<int>& piles) {
         
-        int n=piles.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
+        int n = piles.size();
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(n,vector<int>(2,-1)));
         
-        dfs(0,0,piles,dp);
+        int pa = dfs(0,n-1,0,piles,dp);
         
-        if(dp[0][0]>dp[0][1])
-            return true;
+        int tot =0;
         
-        return false;
+        for(int i=0;i<n;i++)
+            tot+=piles[i];
+        
+        int pb = tot-pa;
+        
+        if(pa>pb)
+            return 1;
+        return 0;
     }
 };
