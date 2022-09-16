426d82e779aac93a1293a8086d142b738d4fbaa0
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-09-16 16:42:45 +0530

    Time: 99 ms (8.24%), Space: 8.2 MB (47.44%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,7 +1,31 @@
 class Solution {
 public:
+    int dfs(int i,int t,vector<int>& piles,vector<vector<int>> &dp)
+    {
+        if(i>=piles.size())
+            return 0;
+        
+        if(dp[i][t]!=-1)
+            return dp[i][t];
+        int k = piles[i]+dfs(i+1,(t+1)%2,piles,dp);
+        
+        int x = piles.back();
+        piles.pop_back();
+        
+        int k1 = x+dfs(i,(t+1)%2,piles,dp);
+        piles.push_back(x);
+        return dp[i][t] = max(k,k1);
+    }
     bool stoneGame(vector<int>& piles) {
         
-        return true;
+        int n=piles.size();
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        
+        dfs(0,0,piles,dp);
+        
+        if(dp[0][0]>dp[1][1])
+            return true;
+        
+        return false;
     }
 };
