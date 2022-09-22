41b04aa65622bf3f4151a113f4b36c49dfd60973
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-09-22 17:48:04 +0530

    Time: 64 ms (74.11%), Space: 28.2 MB (5.80%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
     
-    bool help(int i, int j, string &s, string &p, vector<vector<int>> &dp)
+    bool help(int i, int j, string &s, string &p, vector<vector<int>> &dp,vector<int>&arr)
     {
-      // cout<<i<<" "<<j<<endl;
-        
-        if(i<0&&j<0) return true;
-        
-        if(i>=0&&j<0) return false;
+       
+        if(i<0&&j<0)
+            return true;
         
         if(i<0)
         {
-            while(j>=0)
-            {
-                if(p[j]!='*')
-                    return false;
-                j--;
-            }
+            auto k = upper_bound(arr.begin(),arr.end(),j)-arr.begin();
             
+            if(k==0)
             return true;
+            
+            return false;
         }
-        
+        if(j<0)
+            return false;
+        if(i<0)
+            return false;
         if(dp[i][j]!=-1)
             return dp[i][j];
-        
         if(s[i]==p[j]||p[j]=='?')
-            return dp[i][j] = help(i-1,j-1,s,p,dp);
-        else if(p[j]=='*')
         {
-            return dp[i][j] = (help(i-1,j,s,p,dp)|help(i,j-1,s,p,dp));
+            return dp[i][j] = help(i-1,j-1,s,p,dp,arr);
+        }
+        if(p[j]!='*')
+        {
+            return dp[i][j] =  false;
         }
         else
-            return dp[i][j] = false;
+        {
+           return dp[i][j] = (help(i,j-1,s,p,dp,arr)||help(i-1,j,s,p,dp,arr));
+        }
         
     }
     bool isMatch(string s, string p) {
@@ -39,7 +41,12 @@ public:
         int n = s.size();
         int m = p.size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        
-        return help(s.size()-1,p.size()-1,s,p,dp);
+        vector<int> arr;
+        for(int i=0;i<p.size();i++)
+        {
+            if(p[i]!='*')
+                arr.push_back(i);
+        }
+        return help(s.size()-1,p.size()-1,s,p,dp,arr);
     }
 };
