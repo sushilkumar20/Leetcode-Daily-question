a5a6fbb58238bb69f13910be3004e86713b11a3d
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-10-15 00:45:32 +0530

    Time: 349 ms (16.44%), Space: 16.8 MB (55.44%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,62 +1,36 @@
 class Solution {
 public:
-    static bool cmp(string &s1,string &s2)
+    static bool cmp(string &s1, string &s2)
     {
         if(s1.size()<s2.size())
             return 1;
         
         return 0;
     }
-    
-    bool compare(string s1, string s2)
-    {
-        if(s2.size()+1!=s1.size())
-            return 0;
-        
-        int first =0;
-        int second =0;
-        
-        while(first<s1.size())
-        {
-            if(s1[first]==s2[second])
-            {
-                first++;
-                second++;
-            }
-            else
-            {
-                first++;
-            }
-        }
-        
-        if(first==s1.size()&&second==s2.size())
-            return 1;
-        
-        return 0;
-    }
     int longestStrChain(vector<string>& words) {
         
-        int n = words.size();
-        vector<int> dp(n,1);
-    
+        map<string,int> dp;
         
         sort(words.begin(),words.end(),cmp);
         
-        for(int i=1;i<n;i++)
+        int mx = 1;
+        for(int i=0;i<words.size();i++)
         {
-          for(int j=0;j<i;j++)
-          {
-              if(compare(words[i],words[j])&&dp[i]<dp[j]+1)
-              {
-                  dp[i]=dp[j]+1;
-              }
-          }
+            dp[words[i]] = 1;
+            
+            for(int j=0;j<words[i].size();j++)
+            {
+                string s1 = words[i].substr(0,j);
+                s1+=words[i].substr(j+1);
+                
+                if(dp.find(s1)!=dp.end())
+                {
+                    dp[words[i]] = max(dp[words[i]],1+dp[s1]);
+                }
+                
+            }
+            mx=max(mx,dp[words[i]]);
         }
-        
-        int mx = dp[0];
-        
-        for(int i=1;i<n;i++)
-            mx=max(mx,dp[i]);
         
         return mx;
     }
