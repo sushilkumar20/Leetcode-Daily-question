b5a1ff18769727d99188c5070f881baf70abe919
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-10-15 00:39:26 +0530

    Time: 2502 ms (5.01%), Space: 634.8 MB (5.14%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,32 +1,62 @@
 class Solution {
 public:
-    static bool cmp(string a, string b){
-        return a.size()<b.size();
+    static bool cmp(string &s1,string &s2)
+    {
+        if(s1.size()<s2.size())
+            return 1;
+        
+        return 0;
     }
     
+    bool compare(string s1, string s2)
+    {
+        if(s2.size()+1!=s1.size())
+            return 0;
+        
+        int first =0;
+        int second =0;
+        
+        while(first<s1.size())
+        {
+            if(s1[first]==s2[second])
+            {
+                first++;
+                second++;
+            }
+            else
+            {
+                first++;
+            }
+        }
+        
+        if(first==s1.size()&&second==s2.size())
+            return 1;
+        
+        return 0;
+    }
     int longestStrChain(vector<string>& words) {
         
+        int n = words.size();
+        vector<int> dp(n,1);
+    
+        
         sort(words.begin(),words.end(),cmp);
-        int n=words.size();
-        map<string,int> dp;
-        int mx=1;
-        for(int i=0;i<n;i++)
+        
+        for(int i=1;i<n;i++)
         {
-           //string curr = words[i];
-            dp[words[i]]=1;
-            for(int j=0;j<words[i].size();j++)
-            {
-                string pre = words[i].substr(0,j)+words[i].substr(j+1);
-                //cout<<pre<<endl;
-                if(dp.find(pre)!=dp.end())
-                {
-                    //cout<<words[i]<<endl;
-                    dp[words[i]]=max(dp[words[i]],dp[pre]+1);
-                }
-            }
-            
-            mx=max(mx,dp[words[i]]);
+          for(int j=0;j<i;j++)
+          {
+              if(compare(words[i],words[j])&&dp[i]<dp[j]+1)
+              {
+                  dp[i]=dp[j]+1;
+              }
+          }
         }
+        
+        int mx = dp[0];
+        
+        for(int i=1;i<n;i++)
+            mx=max(mx,dp[i]);
         
         return mx;
     }
