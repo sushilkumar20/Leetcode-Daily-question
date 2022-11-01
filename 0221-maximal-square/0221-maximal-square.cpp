eb84334c7bb60407373712ca82f0f8626d14f88e
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-11-02 01:29:32 +0530

    Time: 693 ms (5.01%), Space: 20.8 MB (69.91%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,79 +1,36 @@
 class Solution {
 public:
-    void calculateMax(vector<int>&height,int &mx)
-    {
-        int n = height.size();
-        vector<int> nextSm(n,n);
-        vector<int> prvSm(n,-1);
-        
-        stack<int> st;
-        for(int i=0;i<n;i++)
-        {
-            while(st.size()&&height[i]<height[st.top()]){
-                nextSm[st.top()]=i;
-                st.pop();
-            }
-            
-            st.push(i);
-        }
-        
-        while(st.size())
-            st.pop();
-        
-        for(int i=n-1;i>=0;i--)
-        {
-            while(st.size()&&height[i]<height[st.top()]){
-                prvSm[st.top()]=i;
-                st.pop();
-            }
-            
-            st.push(i);
-        }
-        
-        
-        int mx1 = 0;
-        
-        for(int i=0;i<n;i++)
-        {
-            // cout<<height[i]<<" ";
-            int width = abs(nextSm[i]-prvSm[i]-1);
-            int d = min(width,height[i]);
-           
-            mx1=max(mx1,d*d);
-        }
-        
-//         cout<<endl;
-//         for(int i=0;i<n;i++)
-//         {
-//             cout<<nextSm[i]<<" "<<prvSm[i]<<endl;
-//         }
-       
-//         cout<<mx1<<endl;
-        
-        mx=max(mx,mx1);
-    }
     int maximalSquare(vector<vector<char>>& matrix) {
         
-        int n = matrix.size();
+        int n=  matrix.size();
         int m = matrix[0].size();
-        
-        int mx = 0;
-        
-        vector<int> height(m,0);
-        
-        for(int i=0;i<matrix.size();i++)
-        {
-            for(int j=0;j<matrix[i].size();j++)
-            {
-                if(matrix[i][j]=='0')
-                    height[j]=0;
-                else
-                    height[j]++;
-            }
+        int mx =0;
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
             
-            calculateMax(height,mx);
+            for(int i=1;i<=n;i++)
+            {
+                for(int j=1;j<=m;j++)
+                {
+                    if(matrix[i-1][j-1]=='1')
+                    {
+                       dp[i][j]=1+min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]});
+                        
+                        mx=max(mx,dp[i][j]);
+                    }
+                    
+                }
+                
+            }
+        
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<endl;
         }
         
-        return mx;
+        return mx*mx;
     }
 };
