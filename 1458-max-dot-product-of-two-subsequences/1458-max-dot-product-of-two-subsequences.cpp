faf1c395a41837512e7a24fdf157298381ad52f0
Sushil Kumar <71508998+sushilkumar20@users.noreply.github.com>
2022-11-03 12:42:14 +0530

    Time: 103 ms (23.78%), Space: 13.1 MB (58.96%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,26 +1,42 @@
 class Solution {
 public:
-    int help(int i, int j, int k,vector<int>&nums1,vector<int>&nums2,vector<vector<vector<int>>> &dp)
+    int help(int i, int j,bool&ok, vector<int>&nums1, vector<int>&nums2, vector<vector<int>>&dp)
     {
-        
         if(i==nums1.size()||j==nums2.size())
-        {
-            if(k)
-                return 0;
-            return -1*1e4;
-        }
+            return 0;
         
-        if(dp[i][j][k]!=-1)
-            return dp[i][j][k];
-        return dp[i][j][k]=max({nums1[i]*nums2[j]+help(i+1,j+1,1,nums1,nums2,dp),help(i+1,j,k,nums1,nums2,dp),help(i,j+1,k,nums1,nums2,dp)});
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int tak = nums1[i]*nums2[j]+help(i+1,j+1,ok,nums1,nums2,dp);
+        int notTak = max(help(i+1,j,ok,nums1,nums2,dp),help(i,j+1,ok,nums1,nums2,dp));
+        
+        dp[i][j]=max(tak,notTak);
+        
+        if(dp[i][j]==tak)
+            ok=1;
+        return dp[i][j];
     }
-    
-    
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         
-        vector<vector<vector<int>>> dp(nums1.size(),vector<vector<int>>(nums2.size(),vector<int>(2,-1)));
+        bool ok = 0;
         
-        // cout<<dp[0][0][0]<<endl;
-        return help(0,0,0,nums1,nums2,dp);
+        vector<vector<int>> dp(nums1.size(),vector<int>(nums2.size(),-1));
+        int ans = help(0,0,ok,nums1,nums2,dp);
+        
+        if(ok)
+            return ans;
+        
+       
+        ans = INT_MIN;
+        
+        for(int i=0;i<nums1.size();i++)
+        {
+            for(int j=0;j<nums2.size();j++)
+            {
+                ans=max(ans,nums1[i]*nums2[j]);
+            }
+        }
+        
+        return ans;
     }
 };
